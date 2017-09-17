43e9626277fe9811ad977dff13b1a4679edadb35
Lucas <Lucas@THEMAZE>
2017-09-16 21:24:46 -0300

    Estrutura de Projeto Maven

---
+++ utilitarios/src/main/java/com/borghi/utilitarios/App.java
@@ -0,0 +1,13 @@
+package com.borghi.utilitarios;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
